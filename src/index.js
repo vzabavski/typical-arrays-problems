c613e3efd77ffaa616572e0261490f7fffca54a7
vzabavski <vzabavski123@gmail.com>
2020-09-29 12:45:17 +0300

    feat: add max min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  var n = array.length;
+    
+  if (n === 0 || array === 0) {
+    return 0;
+  } else {
+    for (var i = 0; i < n; i++)
+     { var v = array[ i ], j = i-1;
+       while (j >= 0 && array[j] > v)
+        { array[j+1] = array[j]; j--; }
+       array[j+1] = v;
+     }                    
+    return array[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  var n = array.length;
+
+  if (n === 0 || array === 0) {
+    return 0;
+  } else {
+    for (var i = 0; i < n; i++)
+     { var v = array[ i ], j = i-1;
+       while (j >= 0 && array[j] > v)
+        { array[j+1] = array[j]; j--; }
+       array[j+1] = v;
+     }                    
+    return array[n-1];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  let avg = 0;
+  
+  if (array.length === 0 || array === 0 ){
+      return 0;
+  } else {
+      for (i = 0; i < array.length; i++) {
+          avg += array[i];
+      }
+    return avg/array.length;
+  }
 }
